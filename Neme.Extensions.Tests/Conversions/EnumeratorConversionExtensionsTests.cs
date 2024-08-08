3e848d46ba64cac4d0acdeb807f4c5a79363aa0d
Simona Koníčková <neme112@outlook.com>
2024-08-08 19:46:59 +0200

    Add EnumeratorConversionExtensions

---
+++ Neme.Extensions.Tests/Conversions/EnumeratorConversionExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+
+namespace Neme.Extensions.Conversions.Tests;
+
+public sealed class EnumeratorConversionExtensionsTests
+{
+    [Fact]
+    public void TestAsNonGeneric()
+    {
+        var enumerator = (IEnumerator<int>)new List<int> { 1, 2, 3 }.GetEnumerator();
+        var asNonGeneric = enumerator.AsNonGeneric();
+        Assert.Same(enumerator, asNonGeneric);
+    }
+
+    [Fact]
+    public void TestAsGeneric_AlreadyGeneric()
+    {
+        var enumerator = (IEnumerator)new List<int> { 1, 2, 3 }.GetEnumerator();
+        var asGeneric = enumerator.AsGeneric<int>();
+        Assert.Same(enumerator, asGeneric);
+    }
+
+    [Fact]
+    public void TestAsGeneric_Custom()
+    {
+        var enumerator = new CustomNonGenericEnumerator().AsGeneric<int>();
+        Enumerate();
+        enumerator.Reset();
+        Enumerate();
+        enumerator.Dispose();
+        Enumerate();
+
+        var enumerator2 = enumerator.AsGeneric<int>();
+        Assert.Same(enumerator, enumerator2);
+
+        void Enumerate()
+        {
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(1, enumerator.Current);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(2, enumerator.Current);
+
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+
+    [Fact]
+    public void TestAsGeneric_WrongType()
+    {
+        var enumerator = new CustomNonGenericEnumerator().AsGeneric<string>();
+        Assert.True(enumerator.MoveNext());
+        Assert.Throws<InvalidCastException>(() => enumerator.Current);
+    }
+
+    private sealed class CustomNonGenericEnumerator : IEnumerator
+    {
+        private readonly int[] _array = [1, 2];
+        private int _nextPosition = 0;
+
+        public object Current =>
+            _array[_nextPosition - 1];
+
+        public bool MoveNext()
+        {
+            if (_nextPosition >= _array.Length)
+                return false;
+
+            ++_nextPosition;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _nextPosition = 0;
+        }
+    }
+}
---
+++ Neme.Extensions/Conversions/EnumeratorConversionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace Neme.Extensions.Conversions;
+
+public static class EnumeratorConversionExtensions
+{
+    public static IEnumerator AsNonGeneric<T>(this IEnumerator<T> enumerator) =>
+        enumerator;
+
+    public static IEnumerator<T> AsGeneric<T>(this IEnumerator enumerator) =>
+        enumerator switch
+        {
+            IEnumerator<T> genericEnumerator => genericEnumerator,
+            _ => new NonGenericEnumeratorWrapper<T>(enumerator),
+        };
+
+    private sealed class NonGenericEnumeratorWrapper<T>(IEnumerator enumerator) : IEnumerator<T>
+    {
+        public T Current => (T)enumerator.Current;
+
+        object IEnumerator.Current => enumerator.Current;
+
+        public bool MoveNext() =>
+            enumerator.MoveNext();
+
+        public void Reset() =>
+            enumerator.Reset();
+
+        public void Dispose() =>
+            enumerator.Reset();
+    }
+}
