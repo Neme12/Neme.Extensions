44769585f051ed25be1baef8b3c0ec6d078b0df0
Simona Koníčková <neme112@outlook.com>
2024-08-24 15:22:48 +0200

    Add CollectionConversionExtensions

---
+++ Neme.Extensions.Tests/Conversions/CollectionConversionExtensionsTests.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using Xunit.Sdk;
+
+namespace Neme.Extensions.Conversions.Tests;
+
+public sealed class CollectionConversionExtensionsTests
+{
+    [Fact]
+    public void TestAsNonGeneric_Same()
+    {
+        var collection = (ICollection<int>)new List<int> { 1, 2, 3 };
+        var asNonGeneric = collection.AsNonGeneric();
+        Assert.Same(collection, asNonGeneric);
+    }
+
+    [Fact]
+    public void TestAsNonGeneric_Wrapper()
+    {
+        var collection = new GenericCollection<int>([1, 2, 3]);
+        var asNonGeneric = collection.AsNonGeneric();
+        Assert.NotSame(collection, asNonGeneric);
+        Assert.Equal(3, asNonGeneric.Count);
+        Assert.False(asNonGeneric.IsSynchronized);
+        Assert.Same(asNonGeneric, asNonGeneric.SyncRoot);
+        Assert.Equal([1, 2, 3], asNonGeneric.Cast<int>());
+
+        var array = new int[3];
+        asNonGeneric.CopyTo(array, 0);
+        Assert.Equal((int[])[1, 2, 3], array);
+    }
+
+    [Fact]
+    public void TestAsNonGeneric_GenericWrapperUnwrapped()
+    {
+        var collection = new NonGenericCollection<int>([1, 2, 3]);
+        var asGeneric = collection.AsGeneric<int>();
+        var asNonGeneric = asGeneric.AsNonGeneric();
+        Assert.Same(collection, asNonGeneric);
+    }
+
+    [Fact]
+    public void TestAsGeneric_Same()
+    {
+        var collection = (ICollection)new List<int> { 1, 2, 3 };
+        var asGeneric = collection.AsGeneric<int>();
+        Assert.Same(collection, asGeneric);
+    }
+
+    [Fact]
+    public void TestAsGeneric_Wrapper()
+    {
+        var collection = new NonGenericCollection<int>([1, 2, 3]);
+        var asGeneric = collection.AsGeneric<int>();
+        Assert.NotSame(collection, asGeneric);
+        Assert.Equal(3, asGeneric.Count);
+        Assert.True(asGeneric.IsReadOnly);
+        Assert.Throws<NotSupportedException>(() => asGeneric.Add(1));
+        Assert.Throws<NotSupportedException>(() => asGeneric.Remove(1));
+        Assert.Throws<NotSupportedException>(() => asGeneric.Clear());
+        Assert.True(asGeneric.Contains(1));
+        Assert.False(asGeneric.Contains(0));
+        Assert.Equal([1, 2, 3], asGeneric);
+
+        var array = new int[3];
+        asGeneric.CopyTo(array, 0);
+        Assert.Equal((int[])[1, 2, 3], array);
+    }
+
+    [Fact]
+    public void TestAsGeneric_NonGenericWrapperUnwrapped()
+    {
+        var collection = new GenericCollection<int>([1, 2, 3]);
+        var asNonGeneric = collection.AsNonGeneric();
+        var asGeneric = asNonGeneric.AsGeneric<int>();
+        Assert.Same(collection, asGeneric);
+    }
+
+    private sealed class NonGenericCollection<T>(List<T> list) : ICollection
+    {
+        public int Count =>
+            ((ICollection)list).Count;
+
+        public bool IsSynchronized =>
+            ((ICollection)list).IsSynchronized;
+
+        public object SyncRoot =>
+            ((ICollection)list).SyncRoot;
+
+        public void CopyTo(Array array, int index) =>
+            ((ICollection)list).CopyTo(array, index);
+
+        public IEnumerator GetEnumerator() =>
+            ((IEnumerable)list).GetEnumerator();
+    }
+
+    private sealed class GenericCollection<T>(List<T> list) : ICollection<T>
+    {
+        public int Count =>
+            ((ICollection<T>)list).Count;
+
+        public bool IsReadOnly =>
+            ((ICollection<T>)list).IsReadOnly;
+
+        public void Add(T item) =>
+            ((ICollection<T>)list).Add(item);
+
+        public bool Remove(T item) =>
+            ((ICollection<T>)list).Remove(item);
+
+        public void Clear() =>
+            ((ICollection<T>)list).Clear();
+
+        public bool Contains(T item) =>
+            ((ICollection<T>)list).Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) =>
+            ((ICollection<T>)list).CopyTo(array, arrayIndex);
+
+        public IEnumerator<T> GetEnumerator() =>
+            ((IEnumerable<T>)list).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            ((IEnumerable)list).GetEnumerator();
+    }
+}
---
+++ Neme.Extensions/Conversions/CollectionConversionExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+namespace Neme.Extensions.Conversions;
+
+public static class CollectionConversionExtensions
+{
+    public static ICollection AsNonGeneric<T>(this ICollection<T> collection) =>
+        collection switch
+        {
+            NonGenericCollectionWrapper<T> nonGenericCollectionWrapper => nonGenericCollectionWrapper.Collection,
+            ICollection nonGenericCollection => nonGenericCollection,
+            _ => new GenericCollectionWrapper<T>(collection),
+        };
+
+    public static ICollection<T> AsGeneric<T>(this ICollection collection) =>
+        collection switch
+        {
+            GenericCollectionWrapper<T> genericCollectionWrapper => genericCollectionWrapper.Collection,
+            ICollection<T> genericCollection => genericCollection,
+            _ => new NonGenericCollectionWrapper<T>(collection),
+        };
+
+    private sealed class NonGenericCollectionWrapper<T>(ICollection collection) : ICollection<T>
+    {
+        public ICollection Collection => collection;
+
+        public int Count =>
+            collection.Count;
+
+        public bool IsReadOnly =>
+            true;
+
+        public void Add(T item) =>
+            throw ReadOnly();
+
+        public bool Remove(T item) =>
+            throw ReadOnly();
+
+        public void Clear() =>
+            throw ReadOnly();
+
+        public bool Contains(T item) =>
+            (collection as IEnumerable).AsGeneric<T>().Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) =>
+            collection.CopyTo(array, arrayIndex);
+
+        public IEnumerator<T> GetEnumerator() =>
+            collection.GetEnumerator().AsGeneric<T>();
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            collection.GetEnumerator();
+
+        private static NotSupportedException ReadOnly() =>
+            new NotSupportedException("Collection is read-only.");
+    }
+
+    private sealed class GenericCollectionWrapper<T>(ICollection<T> collection) : ICollection
+    {
+        public ICollection<T> Collection => collection;
+
+        public int Count =>
+            collection.Count;
+
+        public object SyncRoot =>
+            this;
+
+        public bool IsSynchronized =>
+            false;
+
+        public void CopyTo(Array array, int index) =>
+            collection.CopyTo((T[])array, index);
+
+        public IEnumerator GetEnumerator() =>
+            collection.GetEnumerator();
+    }
+}
