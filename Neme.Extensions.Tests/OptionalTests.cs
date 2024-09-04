52c2610982070959824ab014a3716fd10b1d1c5f
Simona Koníčková <neme112@outlook.com>
2024-09-04 15:54:03 +0200

    Add Optional static class

---
+++ Neme.Extensions.Tests/OptionalTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Neme.Extensions.Tests;
+
+public sealed class OptionalTests
+{
+    [Fact]
+    public void Some()
+    {
+        var optional = Optional.Some(42);
+        Assert.True(optional.HasValue);
+        Assert.Equal(42, optional.Value);
+    }
+
+    [Fact]
+    public void FromNullable_Struct_WithValue()
+    {
+        var optional = Optional.FromNullable((int?)42);
+        Assert.True(optional.HasValue);
+        Assert.Equal(42, optional.Value);
+    }
+
+    [Fact]
+    public void FromNullable_Struct_Null()
+    {
+        var optional = Optional.FromNullable((int?)null);
+        Assert.False(optional.HasValue);
+    }
+
+    [Fact]
+    public void FromNullable_Class_WithValue()
+    {
+        var optional = Optional.FromNullable("hello");
+        Assert.True(optional.HasValue);
+        Assert.Equal("hello", optional.Value);
+    }
+
+    [Fact]
+    public void FromNullable_Class_Null()
+    {
+        var optional = Optional.FromNullable((string?)null);
+        Assert.False(optional.HasValue);
+    }
+
+    [Fact]
+    public void AsNullable_Struct_WithValue()
+    {
+        var optional = new Optional<int>(42);
+        var nullable = optional.AsNullable();
+        Assert.Equal(42, nullable);
+    }
+
+    [Fact]
+    public void AsNullable_Struct_Null()
+    {
+        var optional = default(Optional<int>);
+        var nullable = optional.AsNullable();
+        Assert.Null(nullable);
+    }
+
+    [Fact]
+    public void AsNullable_Class_WithValue()
+    {
+        var optional = new Optional<string>("hello");
+        var nullable = optional.AsNullable();
+        Assert.Equal("hello", nullable);
+    }
+
+    [Fact]
+    public void AsNullable_Class_Null()
+    {
+        var optional = default(Optional<string>);
+        var nullable = optional.AsNullable();
+        Assert.Null(nullable);
+    }
+}
---
+++ Neme.Extensions/Optional.cs
@@ -0,0 +1,31 @@
+﻿namespace Neme.Extensions;
+
+public static class Optional
+{
+    public static Optional<T> Some<T>(T value) =>
+        new(value);
+
+    public static Optional<T> FromNullable<T>(T? value) where T : struct =>
+        value ?? Optional<T>.None;
+
+    public static Optional<T> FromNullable<T>(T? value) where T : class =>
+        value ?? Optional<T>.None;
+}
+
+public static class OptionalValueTypeExtensions
+{
+    public static T? AsNullable<T>(this Optional<T> optional)
+        where T : struct
+    {
+        return optional.TryGetValue(out var value) ? value : null;
+    }
+}
+
+public static class OptionalReferenceTypeExtensions
+{
+    public static T? AsNullable<T>(this Optional<T> optional)
+        where T : class
+    {
+        return optional.TryGetValue(out var value) ? value : null;
+    }
+}
