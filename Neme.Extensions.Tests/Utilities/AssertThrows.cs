5b4d78a7150e632258c8af053143a660707be36f
Simona Koníčková <neme112@outlook.com>
2024-09-05 21:59:29 +0200

    Extract core message from ArgumentException as opposed to using StartsWith

--- Neme.Extensions.Tests/Utilities/AssertThrows.cs
+++ Neme.Extensions.Tests/Utilities/AssertThrows.cs
@@ -11,12 +11,23 @@ internal static class AssertThrows
     public static void Argument_FormatStringNotSupported(Action testCode)
     {
         var e = Assert.Throws<ArgumentException>("format", testCode);
-        Assert.StartsWith("Format string is not supported. The parameter must be null.", e.Message, StringComparison.Ordinal);
+        Assert.Equal("Format string is not supported. The parameter must be null.", GetCoreMessage(e));
     }
 
     public static void Argument_ObjectMustBeOfType(string paramName, string typeName, Action testCode)
     {
         var e = Assert.Throws<ArgumentException>(paramName, testCode);
-        Assert.StartsWith($"Object must be of type {typeName}.", e.Message, StringComparison.Ordinal);
+        Assert.Equal($"Object must be of type {typeName}.", GetCoreMessage(e));
+    }
+
+    private static string GetCoreMessage(ArgumentException exception)
+    {
+        const string testMessage = "<test message>";
+        var testException = new ArgumentException(testMessage, exception.ParamName, exception.InnerException);
+
+        var index = testException.Message.IndexOf(testMessage, StringComparison.Ordinal);
+        var restLength = testException.Message.Length - testMessage.Length;
+
+        return exception.Message[index..^restLength];
     }
 }
