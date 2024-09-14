336a2bfe64b5cceacc69b70edfee12d9fd227afc
Simona Koníčková <neme112@outlook.com>
2024-09-14 20:15:15 +0200

    Split Optional1Tests into multiple files just like the class itself

---
+++ Neme.Extensions.Tests/Optional1Tests.Comparisons.cs
@@ -0,0 +1,232 @@
+﻿using Neme.Extensions.Tests.Utilities;
+using System.Collections;
+
+namespace Neme.Extensions.Tests;
+
+public sealed partial class Optional1Tests
+{
+    [Theory]
+    [InlineData(42, 43, null)]
+#pragma warning disable xUnit1010
+    [InlineData(42f, 43f, float.NaN)]
+#pragma warning restore xUnit1010
+    public void Equals_StronglyTyped<T>(T value1, T value2, T? nan)
+        where T : struct
+    {
+        Assert.True(new Optional<T>().Equals(default));
+        Assert.True(new Optional<T>(value1).Equals(new Optional<T>(value1)));
+        Assert.False(new Optional<T>().Equals(new Optional<T>(value1)));
+        Assert.False(new Optional<T>(value1).Equals(new Optional<T>()));
+        Assert.False(new Optional<T>(value1).Equals(new Optional<T>(value2)));
+
+        if (nan is { } nanValue)
+            Assert.True(new Optional<T>(nanValue).Equals(new Optional<T>(nanValue)));
+    }
+
+    [Fact]
+    public void Equals_CustomComparer()
+    {
+        Assert.True(new Optional<string>().Equals(default, null));
+        Assert.False(new Optional<string>("a").Equals(new Optional<string>("A"), null));
+        Assert.False(new Optional<string>().Equals(new Optional<string>("a"), null));
+        Assert.False(new Optional<string>("a").Equals(new Optional<string>(), null));
+        Assert.False(new Optional<string>("a").Equals(new Optional<string>("b"), null));
+
+        Assert.True(new Optional<string>().Equals(default, StringComparer.OrdinalIgnoreCase));
+        Assert.True(new Optional<string>("a").Equals(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+        Assert.False(new Optional<string>().Equals(new Optional<string>("a"), StringComparer.OrdinalIgnoreCase));
+        Assert.False(new Optional<string>("a").Equals(new Optional<string>(), StringComparer.OrdinalIgnoreCase));
+        Assert.False(new Optional<string>("a").Equals(new Optional<string>("b"), StringComparer.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void Equals_IStructuralEquatable()
+    {
+        Assert.Throws<ArgumentNullException>("comparer", () => ((IStructuralEquatable)new Optional<string>()).Equals(default(Optional<string>), null!));
+
+        Assert.True(((IStructuralEquatable)new Optional<string>()).Equals(default(Optional<string>), StringComparer.OrdinalIgnoreCase));
+        Assert.True(((IStructuralEquatable)new Optional<string>("a")).Equals(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+        Assert.False(((IStructuralEquatable)new Optional<string>()).Equals(new Optional<string>("a"), StringComparer.OrdinalIgnoreCase));
+        Assert.False(((IStructuralEquatable)new Optional<string>("a")).Equals(new Optional<string>(), StringComparer.OrdinalIgnoreCase));
+        Assert.False(((IStructuralEquatable)new Optional<string>("a")).Equals(new Optional<string>("b"), StringComparer.OrdinalIgnoreCase));
+
+        Assert.False(((IStructuralEquatable)new Optional<string>()).Equals(null, StringComparer.OrdinalIgnoreCase));
+        Assert.False(((IStructuralEquatable)new Optional<string>()).Equals(new Optional<int>(), StringComparer.OrdinalIgnoreCase));
+        Assert.False(((IStructuralEquatable)new Optional<string>("a")).Equals("a", StringComparer.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void Equals_Override()
+    {
+        Assert.True(new Optional<int>().Equals((object)default(Optional<int>)));
+        Assert.True(new Optional<int>(42).Equals((object)new Optional<int>(42)));
+        Assert.False(new Optional<int>().Equals((object)new Optional<int>(42)));
+        Assert.False(new Optional<int>(42).Equals((object)new Optional<int>()));
+        Assert.False(new Optional<int>(42).Equals((object)new Optional<int>(43)));
+
+        Assert.False(new Optional<int>().Equals(null));
+        Assert.False(new Optional<int>().Equals(new Optional<string>()));
+        Assert.False(new Optional<int>(5).Equals((object)5));
+    }
+
+    [Fact]
+    public void CompareTo_StronglyTyped()
+    {
+        Assert.Equal(0, new Optional<int>().CompareTo(default));
+        Assert.Equal(0, new Optional<int>(42).CompareTo(new Optional<int>(42)));
+        Assert.Equal(-1, new Optional<int>().CompareTo(new Optional<int>(42)));
+        Assert.Equal(1, new Optional<int>(42).CompareTo(new Optional<int>()));
+        Assert.Equal(-1, new Optional<int>(42).CompareTo(new Optional<int>(43)));
+        Assert.Equal(1, new Optional<int>(43).CompareTo(new Optional<int>(42)));
+    }
+
+    [Fact]
+    public void CompareTo_CustomComparer()
+    {
+        Assert.Equal(0, new Optional<string>().CompareTo(default, null));
+        Assert.Equal(-1, new Optional<string>("a").CompareTo(new Optional<string>("A"), null));
+        Assert.Equal(-1, new Optional<string>().CompareTo(new Optional<string>("a"), null));
+        Assert.Equal(1, new Optional<string>("a").CompareTo(new Optional<string>(), null));
+        Assert.Equal(-1, new Optional<string>("a").CompareTo(new Optional<string>("B"), null));
+        Assert.Equal(1, new Optional<string>("b").CompareTo(new Optional<string>("A"), null));
+
+        Assert.Equal(0, new Optional<string>().CompareTo(default, StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(0, new Optional<string>("a").CompareTo(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(-1, new Optional<string>().CompareTo(new Optional<string>("a"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(1, new Optional<string>("a").CompareTo(new Optional<string>(), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(-1, new Optional<string>("a").CompareTo(new Optional<string>("B"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(1, new Optional<string>("b").CompareTo(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void CompareTo_IStructuralComparable()
+    {
+        Assert.Throws<ArgumentNullException>("comparer", () => ((IStructuralComparable)new Optional<string>()).CompareTo(default(Optional<string>), null!));
+
+        Assert.Equal(0, ((IStructuralComparable)new Optional<string>()).CompareTo(default(Optional<string>), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(0, ((IStructuralComparable)new Optional<string>("a")).CompareTo(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(-1, ((IStructuralComparable)new Optional<string>()).CompareTo(new Optional<string>("a"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(1, ((IStructuralComparable)new Optional<string>("a")).CompareTo(new Optional<string>(), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(-1, ((IStructuralComparable)new Optional<string>("a")).CompareTo(new Optional<string>("B"), StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(1, ((IStructuralComparable)new Optional<string>("b")).CompareTo(new Optional<string>("A"), StringComparer.OrdinalIgnoreCase));
+
+        Assert.Equal(1, ((IStructuralComparable)new Optional<string>()).CompareTo(null, StringComparer.OrdinalIgnoreCase));
+        AssertThrows.Argument_ObjectMustBeOfType("other", "Neme.Extensions.Optional`1[System.String]", () => ((IStructuralComparable)new Optional<string>()).CompareTo(new Optional<int>(), StringComparer.OrdinalIgnoreCase));
+        AssertThrows.Argument_ObjectMustBeOfType("other", "Neme.Extensions.Optional`1[System.String]", () => ((IStructuralComparable)new Optional<string>("a")).CompareTo("a", StringComparer.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void CompareTo_IComparable()
+    {
+        Assert.Equal(0, ((IComparable)new Optional<int>()).CompareTo(default(Optional<int>)));
+        Assert.Equal(0, ((IComparable)new Optional<int>(42)).CompareTo(new Optional<int>(42)));
+        Assert.Equal(-1, ((IComparable)new Optional<int>()).CompareTo(new Optional<int>(42)));
+        Assert.Equal(1, ((IComparable)new Optional<int>(42)).CompareTo(new Optional<int>()));
+        Assert.Equal(-1, ((IComparable)new Optional<int>(42)).CompareTo(new Optional<int>(43)));
+        Assert.Equal(1, ((IComparable)new Optional<int>(43)).CompareTo(new Optional<int>(42)));
+
+        Assert.Equal(1, ((IComparable)new Optional<int>()).CompareTo(null));
+        AssertThrows.Argument_ObjectMustBeOfType("obj", "Neme.Extensions.Optional`1[System.Int32]", () => ((IComparable)new Optional<int>()).CompareTo(new Optional<string>()));
+        AssertThrows.Argument_ObjectMustBeOfType("obj", "Neme.Extensions.Optional`1[System.Int32]", () => ((IComparable)new Optional<int>(5)).CompareTo(5));
+    }
+
+    [Theory]
+    [InlineData(42, 43, null)]
+#pragma warning disable xUnit1010
+    [InlineData(42f, 43f, float.NaN)]
+#pragma warning restore xUnit1010
+    public void EqualityOperator<T>(T value1, T value2, T? nan)
+        where T : struct
+    {
+        Assert.True(new Optional<T>() == default);
+        Assert.True(new Optional<T>(value1) == new Optional<T>(value1));
+        Assert.False(new Optional<T>() == new Optional<T>(value1));
+        Assert.False(new Optional<T>(value1) == new Optional<T>());
+        Assert.False(new Optional<T>(value1) == new Optional<T>(value2));
+
+        if (nan is { } nanValue)
+            Assert.False(new Optional<T>(nanValue) == new Optional<T>(nanValue));
+    }
+
+    [Theory]
+    [InlineData(42, 43, null)]
+#pragma warning disable xUnit1010
+    [InlineData(42f, 43f, float.NaN)]
+#pragma warning restore xUnit1010
+    public void InequalityOperator<T>(T value1, T value2, T? nan)
+        where T : struct
+    {
+        Assert.False(new Optional<T>() != default);
+        Assert.False(new Optional<T>(value1) != new Optional<T>(value1));
+        Assert.True(new Optional<T>() != new Optional<T>(value1));
+        Assert.True(new Optional<T>(value1) != new Optional<T>());
+        Assert.True(new Optional<T>(value1) != new Optional<T>(value2));
+
+        if (nan is { } nanValue)
+            Assert.True(new Optional<T>(nanValue) != new Optional<T>(nanValue));
+    }
+
+    [Fact]
+    public void EqualityOperator_CustomComparable()
+    {
+        Assert.True(new Optional<CustomComparable>() == default);
+        Assert.True(new Optional<CustomComparable>(new(1)) == new Optional<CustomComparable>(new(2)));
+        Assert.False(new Optional<CustomComparable>() == new Optional<CustomComparable>(new(1)));
+        Assert.False(new Optional<CustomComparable>(new(1)) == new Optional<CustomComparable>());
+        Assert.False(new Optional<CustomComparable>(new(2)) == new Optional<CustomComparable>(new(2)));
+    }
+
+    [Fact]
+    public void InequalityOperator_CustomComparable()
+    {
+        Assert.False(new Optional<CustomComparable>() != default);
+        Assert.False(new Optional<CustomComparable>(new(1)) != new Optional<CustomComparable>(new(2)));
+        Assert.True(new Optional<CustomComparable>() != new Optional<CustomComparable>(new(1)));
+        Assert.True(new Optional<CustomComparable>(new(1)) != new Optional<CustomComparable>());
+        Assert.True(new Optional<CustomComparable>(new(-1)) != new Optional<CustomComparable>(new(1)));
+    }
+
+    [Fact]
+    public void GetHashCode_Override()
+    {
+        Assert.Equal(-1, new Optional<int>().GetHashCode());
+        Assert.Equal(0, new Optional<string?>(null).GetHashCode());
+#pragma warning disable CA1307 // Specify StringComparison for clarity
+        Assert.Equal("hello".GetHashCode(), new Optional<string>("hello").GetHashCode());
+#pragma warning restore CA1307 // Specify StringComparison for clarity
+    }
+
+    [Fact]
+    public void GetHashCode_CustomComparer()
+    {
+        Assert.Equal(-1, new Optional<string>().GetHashCode(null));
+        Assert.Equal(EqualityComparer<string>.Default.GetHashCode("hello"), new Optional<string>("hello").GetHashCode(null));
+
+        Assert.Equal(-1, new Optional<string>().GetHashCode(StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(StringComparer.OrdinalIgnoreCase.GetHashCode("hello"), new Optional<string>("hello").GetHashCode(StringComparer.OrdinalIgnoreCase));
+    }
+
+    [Fact]
+    public void GetHashCode_IStructuralEquatable()
+    {
+        Assert.Throws<ArgumentNullException>("comparer", () => ((IStructuralEquatable)new Optional<string>()).GetHashCode(null!));
+
+        Assert.Equal(-1, ((IStructuralEquatable)new Optional<string>()).GetHashCode(StringComparer.OrdinalIgnoreCase));
+        Assert.Equal(StringComparer.OrdinalIgnoreCase.GetHashCode("hello"), ((IStructuralEquatable)new Optional<string>("hello")).GetHashCode(StringComparer.OrdinalIgnoreCase));
+    }
+
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+#pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+    private readonly struct CustomComparable(int value)
+#pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+#pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+    {
+        public int Value => value;
+
+        public static bool operator ==(CustomComparable a, CustomComparable b) =>
+            a.Value == 1;
+
+        public static bool operator !=(CustomComparable a, CustomComparable b) =>
+            a.Value == -1;
+    }
+}
---
+++ Neme.Extensions.Tests/Optional1Tests.Formatting.cs
@@ -0,0 +1,388 @@
+﻿using Neme.Extensions.Tests.Utilities;
+using System.Globalization;
+using System.Numerics;
+
+namespace Neme.Extensions.Tests;
+
+public sealed partial class Optional1Tests
+{
+    [Fact]
+    public void ToString_None()
+    {
+        var optional = default(Optional<int>);
+
+        Assert.Equal("None", optional.ToString());
+    }
+
+    [Fact]
+    public void ToString_Some()
+    {
+        var optional = new Optional<double>(42.2);
+
+        Assert.Equal($"Some {{ {optional.Value} }}", optional.ToString());
+    }
+
+    [Fact]
+    public void ToString_Some_Null()
+    {
+        var optional = new Optional<object?>(null);
+
+        Assert.Equal("Some { }", optional.ToString());
+    }
+
+    [Fact]
+    public void ToString_IFormattable_None()
+    {
+        var optional = default(Optional<int>);
+
+        AssertToStringIFormattable(optional, "None", null);
+        AssertToStringIFormattable(optional, "None", CultureInfo.InvariantCulture);
+        AssertToStringIFormattable(optional, "None", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void ToString_IFormattable_Some()
+    {
+        var optional = new Optional<double>(42.2);
+
+        AssertToStringIFormattable(optional, $"Some {{ {optional.Value} }}", null);
+        AssertToStringIFormattable(optional, "Some { 42.2 }", CultureInfo.InvariantCulture);
+        AssertToStringIFormattable(optional, "Some { 42,2 }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void ToString_IFormattable_Some_Null()
+    {
+        var optional = new Optional<object?>(null);
+
+        AssertToStringIFormattable(optional, "Some { }", null);
+        AssertToStringIFormattable(optional, "Some { }", CultureInfo.InvariantCulture);
+        AssertToStringIFormattable(optional, "Some { }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    private static void AssertToStringIFormattable<T>(Optional<T> optional, string expected, CultureInfo? culture)
+    {
+        Assert.Equal(expected, optional.ToString(null, culture));
+        Assert.Equal(expected, optional.ToString("", culture));
+        AssertThrows.Argument_FormatStringNotSupported(() => optional.ToString("x", culture));
+    }
+
+#if NET6_0_OR_GREATER
+    [Fact]
+    public void ToString_ISpanFormattable_None()
+    {
+        var optional = default(Optional<int>);
+
+        AssertToStringISpanFormattable(optional, "None", null);
+        AssertToStringISpanFormattable(optional, "None", CultureInfo.InvariantCulture);
+        AssertToStringISpanFormattable(optional, "None", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void ToString_ISpanFormattable_Some()
+    {
+        var optional = new Optional<double>(42.2);
+
+        AssertToStringISpanFormattable(optional, $"Some {{ {optional.Value} }}", null);
+        AssertToStringISpanFormattable(optional, "Some { 42.2 }", CultureInfo.InvariantCulture);
+        AssertToStringISpanFormattable(optional, "Some { 42,2 }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void ToString_ISpanFormattable_Some_Null()
+    {
+        var optional = new Optional<object?>(null);
+
+        AssertToStringISpanFormattable(optional, "Some { }", null);
+        AssertToStringISpanFormattable(optional, "Some { }", CultureInfo.InvariantCulture);
+        AssertToStringISpanFormattable(optional, "Some { }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    private static void AssertToStringISpanFormattable<T>(Optional<T> optional, string expected, CultureInfo? culture)
+    {
+        {
+            Span<char> destination = stackalloc char[expected.Length];
+            Assert.True(optional.TryFormat(destination, out var charsWritten, default, culture));
+            Assert.Equal(expected, destination);
+            Assert.Equal(expected.Length, charsWritten);
+        }
+
+        {
+            Span<char> destination = stackalloc char[expected.Length + 5];
+            Assert.True(optional.TryFormat(destination, out var charsWritten, default, culture));
+            Assert.Equal(expected, destination[..expected.Length]);
+            Assert.Equal(stackalloc char[5], destination[expected.Length..]);
+            Assert.Equal(expected.Length, charsWritten);
+        }
+
+        if (expected.Length > 0)
+        {
+            Span<char> destination = stackalloc char[expected.Length - 1];
+            Assert.False(optional.TryFormat(destination, out var charsWritten, default, culture));
+            Assert.Equal(0, charsWritten);
+        }
+
+        AssertThrows.Argument_FormatStringNotSupported(() => optional.TryFormat(default, out _, "x", culture));
+    }
+#endif
+
+    [Fact]
+    public void Parse_Null()
+    {
+        AssertDoesNotParse<int>(null!, null, null);
+        AssertDoesNotParse<int>(null!, null, CultureInfo.InvariantCulture);
+        AssertDoesNotParse<int>(null!, null, CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_None()
+    {
+        AssertParses<int>(default, "None", null);
+        AssertParses<int>(default, "None", CultureInfo.InvariantCulture);
+        AssertParses<int>(default, "None", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_Some()
+    {
+        AssertParses<double>(new(42.2), $"Some {{ {42.2} }}", null);
+        AssertParses<double>(new(42.2), "Some { 42.2 }", CultureInfo.InvariantCulture);
+        AssertParses<double>(new(42.2), "Some { 42,2 }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_Version()
+    {
+        AssertParses<Version>(new(new(1, 2, 3, 4)), "Some { 1.2.3.4 }", null);
+        AssertParses<Version>(new(new(1, 2, 3, 4)), "Some { 1.2.3.4 }", CultureInfo.InvariantCulture);
+        AssertParses<Version>(new(new(1, 2, 3, 4)), "Some { 1.2.3.4 }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_Some_EmptyString()
+    {
+        AssertParses<string?>(new(""), "Some {  }", null);
+        AssertParses<string?>(new(""), "Some {  }", CultureInfo.InvariantCulture);
+        AssertParses<string?>(new(""), "Some {  }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_Some_Null()
+    {
+        AssertParses<string?>(new(null), "Some { }", null);
+        AssertParses<string?>(new(null), "Some { }", CultureInfo.InvariantCulture);
+        AssertParses<string?>(new(null), "Some { }", CultureInfo.GetCultureInfo("de"));
+    }
+
+    [Fact]
+    public void Parse_InvalidStrings()
+    {
+        AssertDoesNotParse<int>("", null, null);
+        AssertDoesNotParse<int>("42", null, null);
+        AssertDoesNotParse<int>("{ }", null, null);
+        AssertDoesNotParse<int>("{ 42 }", null, null);
+        AssertDoesNotParse<int>("Some", null, null);
+        AssertDoesNotParse<int>("Some {}", null, null);
+        AssertDoesNotParse<int>("Some { x }", "x", null);
+        AssertDoesNotParse<int>("SOME { 42 }", null, null);
+    }
+
+    [Fact]
+    public void Parse_TestNumberStyles()
+    {
+        TestIsInteger<ushort>(null);
+        TestIsInteger<short>(-1);
+        TestIsInteger<uint>(null);
+        TestIsInteger<int>(-1);
+        TestIsInteger<ulong>(null);
+        TestIsInteger<long>(-1);
+#if NET5_0_OR_GREATER
+        TestIsInteger<nuint>(null);
+        TestIsInteger<nint>(-1);
+#endif
+        TestIsInteger<BigInteger>(BigInteger.MinusOne);
+
+        TestIsFloatAndAllowThousands<float>(-1f, 42.2f, 10_000f);
+        TestIsFloatAndAllowThousands<double>(-1d, 42.2d, 10_000d);
+
+        TestIsNumber<decimal>(-1m, 42.2m, 10_000m);
+
+        static void TestIsInteger<T>(T? negativeOne)
+            where T : struct
+        {
+            // NumberStyles.AllowLeadingWhite and NumberStyles.AllowTrailingWhite are included.
+            AssertParses<T>(new(default), "Some {  0  }", null);
+
+            if (negativeOne is not null)
+            {
+                // NumberStyles.AllowLeadingSign is included
+                AssertParses<T>(new(negativeOne.Value), "Some { -1 }", null);
+            }
+
+            // NumberStyles.AllowDecimalPoint is *not* included.
+            AssertDoesNotParse<T>("Some { 42.2 }", "42.2", null);
+
+            // NumberStyles.AllowThousands is *not* included.
+            AssertDoesNotParse<T>("Some { 10,000 }", "10,000", null);
+
+            // NumberStyles.AllowExponent is *not* included.
+            AssertDoesNotParse<T>("Some { 4.22e+1 }", "4.22e+1", null);
+
+            // NumberStyles.AllowParentheses is *not* included.
+            AssertDoesNotParse<T>("Some { (42) }", "(42)", null);
+
+            // NumberStyles.AllowHexSpecifier is *not* included.
+            AssertDoesNotParse<T>("Some { f }", "f", null);
+        }
+
+        static void TestIsFloatAndAllowThousands<T>(T negativeOne, T value1, T value2)
+            where T : struct
+        {
+            // NumberStyles.AllowLeadingWhite and NumberStyles.AllowTrailingWhite are included.
+            AssertParses<T>(new(default), "Some {  0  }", null);
+
+            // NumberStyles.AllowLeadingSign is included
+            AssertParses<T>(new(negativeOne), "Some { -1 }", null);
+
+            // NumberStyles.AllowDecimalPoint is included.
+            AssertParses<T>(new(value1), "Some { 42.2 }", null);
+
+            // NumberStyles.AllowThousands is included.
+            AssertParses<T>(new(value2), "Some { 10,000 }", null);
+
+            // NumberStyles.AllowExponent is included.
+            AssertParses<T>(new(value1), "Some { 4.22e+1 }", null);
+
+            // NumberStyles.AllowParentheses is *not* included.
+            AssertDoesNotParse<T>("Some { (42) }", "(42)", null);
+
+            // NumberStyles.AllowHexSpecifier is *not* included.
+            AssertDoesNotParse<T>("Some { f }", "f", null);
+        }
+
+        static void TestIsNumber<T>(T negativeOne, T value1, T value2)
+            where T : struct
+        {
+            // NumberStyles.AllowLeadingWhite and NumberStyles.AllowTrailingWhite are included.
+            AssertParses<T>(new(default), "Some {  0  }", null);
+
+            // NumberStyles.AllowLeadingSign is included
+            AssertParses<T>(new(negativeOne), "Some { -1 }", null);
+
+            // NumberStyles.AllowDecimalPoint is included.
+            AssertParses<T>(new(value1), "Some { 42.2 }", null);
+
+            // NumberStyles.AllowThousands is included.
+            AssertParses<T>(new(value2), "Some { 10,000 }", null);
+
+            // NumberStyles.AllowExponent is *not* included.
+            AssertDoesNotParse<T>("Some { 4.22e+1 }", "4.22e+1", null);
+
+            // NumberStyles.AllowParentheses is *not* included.
+            AssertDoesNotParse<T>("Some { (42) }", "(42)", null);
+
+            // NumberStyles.AllowHexSpecifier is *not* included.
+            AssertDoesNotParse<T>("Some { f }", "f", null);
+        }
+    }
+
+    private static void AssertDoesNotParse<T>(string input, string? nestedInput, IFormatProvider? provider)
+    {
+        var parseSpan = ShouldParseSpan<T>();
+
+        if (provider is null)
+        {
+#pragma warning disable CA1305 // Specify IFormatProvider
+            if (input is null)
+            {
+                Assert.Throws<ArgumentNullException>("s", () => Optional<T>.Parse(input!));
+            }
+            else
+            {
+                AssertThrows.Format(input, nestedInput, () => Optional<T>.Parse(input));
+
+                if (parseSpan)
+                    AssertThrows.Format(input, nestedInput, () => Optional<T>.Parse(input.AsSpan()));
+            }
+
+            Assert.False(Optional<T>.TryParse(input, out var resultWithoutProvider1));
+            Assert.Equal(default, resultWithoutProvider1);
+
+            if (parseSpan)
+            {
+                Assert.False(Optional<T>.TryParse(input.AsSpan(), out var resultWithoutProvider2));
+                Assert.Equal(default, resultWithoutProvider2);
+            }
+#pragma warning restore CA1305 // Specify IFormatProvider
+        }
+
+        if (input is null)
+        {
+            Assert.Throws<ArgumentNullException>("s", () => Optional<T>.Parse(input!, provider));
+        }
+        else
+        {
+            AssertThrows.Format(input, nestedInput, () => Optional<T>.Parse(input, provider));
+
+            if (parseSpan)
+                AssertThrows.Format(input, nestedInput, () => Optional<T>.Parse(input.AsSpan(), provider));
+        }
+
+        Assert.False(Optional<T>.TryParse(input, provider, out var resultWithProvider1));
+        Assert.Equal(default, resultWithProvider1);
+
+        if (parseSpan)
+        {
+            Assert.False(Optional<T>.TryParse(input.AsSpan(), provider, out var resultWithProvider2));
+            Assert.Equal(default, resultWithProvider2);
+        }
+    }
+
+    private static void AssertParses<T>(Optional<T> expected, string input, IFormatProvider? provider)
+    {
+        var parseSpan = ShouldParseSpan<T>();
+
+        if (provider is null)
+        {
+#pragma warning disable CA1305 // Specify IFormatProvider
+            Assert.Equal(expected, Optional<T>.Parse(input));
+
+            if (parseSpan)
+                Assert.Equal(expected, Optional<T>.Parse(input.AsSpan()));
+#pragma warning restore CA1305 // Specify IFormatProvider
+
+            Assert.True(Optional<T>.TryParse(input, out var resultWithoutProvider1));
+            Assert.Equal(expected, resultWithoutProvider1);
+
+            if (parseSpan)
+            {
+                Assert.True(Optional<T>.TryParse(input.AsSpan(), out var resultWithoutProvider2));
+                Assert.Equal(expected, resultWithoutProvider2);
+            }
+        }
+
+        Assert.Equal(expected, Optional<T>.Parse(input, provider));
+
+        if (parseSpan)
+            Assert.Equal(expected, Optional<T>.Parse(input.AsSpan(), provider));
+
+        Assert.True(Optional<T>.TryParse(input, provider, out var resultWithProvider1));
+        Assert.Equal(expected, resultWithProvider1);
+
+        if (parseSpan)
+        {
+            Assert.True(Optional<T>.TryParse(input.AsSpan(), provider, out var resultWithProvider2));
+            Assert.Equal(expected, resultWithProvider2);
+        }
+
+    }
+
+    private static bool ShouldParseSpan<T>()
+    {
+#if NETCOREAPP2_1_OR_GREATER
+        return true;
+#else
+        return false;
+#endif
+    }
+}
