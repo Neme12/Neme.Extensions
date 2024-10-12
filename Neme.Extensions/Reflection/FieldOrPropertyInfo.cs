ca82327a54e1f79abe6d9d5bd7149293c3d36f55
Simona Koníčková <neme112@outlook.com>
2024-10-12 23:03:41 +0200

    FieldOrPropertyInfo changes

--- Neme.Extensions/Reflection/FieldOrPropertyInfo.cs
+++ Neme.Extensions/Reflection/FieldOrPropertyInfo.cs
@@ -1,114 +1,84 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Neme.Extensions.Reflection;
 
-internal readonly struct FieldOrPropertyInfo
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+#pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+internal abstract class FieldOrPropertyInfo : MemberInfo
+#pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+#pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 {
-    private readonly MemberInfo _memberInfo;
-
-    public FieldOrPropertyInfo(MemberInfo memberInfo)
+    private FieldOrPropertyInfo()
     {
-        if (!IsValidMember(memberInfo))
-            throw new ArgumentException(null, nameof(memberInfo));
-
-        _memberInfo = memberInfo;
     }
-
-    public FieldOrPropertyInfo(FieldInfo fieldInfo)
-    {
-        _memberInfo = fieldInfo;
-    }
-
-    public FieldOrPropertyInfo(PropertyInfo propertyInfo)
-    {
-        if (!IsValidProperty(propertyInfo))
-            throw new ArgumentException(null, nameof(propertyInfo));
-
-        _memberInfo = propertyInfo;
-    }
-
-    public MemberTypes MemberType =>
-        _memberInfo.MemberType;
-
-    public string Name =>
-        _memberInfo.Name;
-
-    public Type? DeclaringType =>
-        _memberInfo.DeclaringType;
-
-    public Type? ReflectedType =>
-        _memberInfo.ReflectedType;
 
     public Type Type
     {
         get
         {
-            return _memberInfo switch
+            return (MemberInfo)this switch
             {
                 FieldInfo field => field.FieldType,
                 PropertyInfo property => property.PropertyType,
-                _ => throw new ObjectDisposedException(nameof(FieldOrPropertyInfo)),
+                _ => throw new InvalidOperationException(),
             };
         }
     }
 
-    public Module Module =>
-        _memberInfo.Module;
+    public bool CanRead =>
+        (MemberInfo)this switch
+        {
+            FieldInfo field => true,
+            PropertyInfo property => property.CanRead,
+            _ => throw new InvalidOperationException(),
+        };
 
-#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-    public bool HasSameMetadataDefinitionAs(MemberInfo other) =>
-        _memberInfo.HasSameMetadataDefinitionAs(other);
-#endif
-
-    public bool IsDefined(Type attributeType, bool inherit) =>
-        _memberInfo.IsDefined(attributeType, inherit);
-
-    public object[] GetCustomAttributes(bool inherit) =>
-        _memberInfo.GetCustomAttributes(inherit);
-
-    public object[] GetCustomAttributes(Type attributeType, bool inherit) =>
-        _memberInfo.GetCustomAttributes(attributeType, inherit);
-
-    public IEnumerable<CustomAttributeData> CustomAttributes =>
-        _memberInfo.CustomAttributes;
-
-    public IList<CustomAttributeData> GetCustomAttributesData() =>
-        _memberInfo.GetCustomAttributesData();
-
-#if NETCOREAPP3_0_OR_GREATER
-    public bool IsCollectible =>
-        _memberInfo.IsCollectible;
-#endif
-
-    public int MetadataToken =>
-        _memberInfo.MetadataToken;
+    public bool CanWrite =>
+        (MemberInfo)this switch
+        {
+            FieldInfo field => !field.IsLiteral,
+            PropertyInfo property => property.CanWrite,
+            _ => throw new InvalidOperationException(),
+        };
 
     public bool IsLiteral =>
-        _memberInfo is FieldInfo { IsLiteral: true };
+        (MemberInfo)this is FieldInfo { IsLiteral: true };
 
     public bool IsReadOnly =>
-        _memberInfo switch
+        (MemberInfo)this switch
         {
             FieldInfo field => field.IsLiteral || field.IsInitOnly,
             PropertyInfo property => !property.CanWrite,
-            _ => throw new ObjectDisposedException(nameof(FieldOrPropertyInfo)),
+            _ => throw new InvalidOperationException(),
+        };
+
+    public bool HasParameters =>
+        (MemberInfo)this switch
+        {
+#pragma warning disable CA1508 // Avoid dead conditional code
+            FieldInfo field => false,
+            PropertyInfo property => property.GetIndexParameters().Length > 0,
+#pragma warning restore CA1508 // Avoid dead conditional code
+            _ => throw new InvalidOperationException(),
         };
 
     public object? GetValue(object? obj)
     {
-        return _memberInfo switch
+        return (MemberInfo)this switch
         {
             FieldInfo field => field.GetValue(obj),
             PropertyInfo property => property.GetValue(obj),
-            _ => throw new ObjectDisposedException(nameof(FieldOrPropertyInfo)),
+            _ => throw new InvalidOperationException(),
         };
     }
 
     public void SetValue(object? obj, object? value)
     {
-        switch (_memberInfo)
+        switch ((MemberInfo)this)
         {
             case FieldInfo field:
                 field.SetValue(obj, value);
@@ -117,13 +87,13 @@ internal readonly struct FieldOrPropertyInfo
                 property.SetValue(obj, value);
                 break;
             default:
-                throw new ObjectDisposedException(nameof(FieldOrPropertyInfo));
+                throw new InvalidOperationException();
         }
     }
 
     public void SetValue(object? obj, object? value, BindingFlags invokeAttr, Binder? binder, CultureInfo? culture)
     {
-        switch (_memberInfo)
+        switch ((MemberInfo)this)
         {
             case FieldInfo field:
                 field.SetValue(obj, value, invokeAttr, binder, culture);
@@ -132,46 +102,74 @@ internal readonly struct FieldOrPropertyInfo
                 property.SetValue(obj, value, invokeAttr, binder, null, culture);
                 break;
             default:
-                throw new ObjectDisposedException(nameof(FieldOrPropertyInfo));
+                throw new InvalidOperationException();
         }
     }
 
     public object? GetRawConstantValue()
     {
-        return _memberInfo switch
+        return (MemberInfo)this switch
         {
             FieldInfo field => field.GetRawConstantValue(),
             PropertyInfo property => property.GetRawConstantValue(),
-            _ => throw new ObjectDisposedException(nameof(FieldOrPropertyInfo)),
+            _ => throw new InvalidOperationException(),
         };
     }
 
-    private static bool IsValidMember([NotNullWhen(true)] MemberInfo? memberInfo) =>
-        memberInfo is FieldInfo || memberInfo is PropertyInfo property && IsValidProperty(property);
+    [return: NotNullIfNotNull(nameof(fieldInfo))]
+    public static implicit operator FieldOrPropertyInfo?(FieldInfo? fieldInfo) =>
+        fieldInfo.AsFieldOrPropertyInfo();
 
-    private static bool IsValidProperty(PropertyInfo propertyInfo) =>
-        propertyInfo.CanRead && propertyInfo.GetIndexParameters().Length == 0;
+    [return: NotNullIfNotNull(nameof(propertyInfo))]
+    public static implicit operator FieldOrPropertyInfo?(PropertyInfo? propertyInfo) =>
+        propertyInfo.AsFieldOrPropertyInfo();
 
-    public static FieldOrPropertyInfo? Get(MemberInfo? memberInfo) =>
-        IsValidMember(memberInfo)
-            ? new(memberInfo)
-            : null;
+    [StackTraceHidden]
+    [return: NotNullIfNotNull(nameof(fieldOrPropertyInfo))]
+    public static explicit operator FieldInfo?(FieldOrPropertyInfo? fieldOrPropertyInfo) =>
+        (FieldInfo?)(MemberInfo?)fieldOrPropertyInfo;
 
-    public static explicit operator FieldOrPropertyInfo(MemberInfo memberInfo) =>
-        new(memberInfo);
+    [StackTraceHidden]
+    [return: NotNullIfNotNull(nameof(fieldOrPropertyInfo))]
+    public static explicit operator PropertyInfo?(FieldOrPropertyInfo? fieldOrPropertyInfo) =>
+        (PropertyInfo?)(MemberInfo?)fieldOrPropertyInfo;
 
-    public static implicit operator FieldOrPropertyInfo(FieldInfo fieldInfo) =>
-        new(fieldInfo);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(FieldOrPropertyInfo? left, FieldOrPropertyInfo? right)
+    {
+        // Test "right" first to allow branch elimination when inlined for null checks (== null)
+        // so it can become a simple test
+        if (right is null)
+        {
+            return left is null;
+        }
 
-    public static explicit operator FieldOrPropertyInfo(PropertyInfo propertyInfo) =>
-        new(propertyInfo);
+        // Try fast reference equality and opposite null check prior to calling the slower virtual Equals
+        if (ReferenceEquals(left, right))
+        {
+            return true;
+        }
 
-    public static implicit operator MemberInfo(FieldOrPropertyInfo fieldOrPropertyInfo) =>
-        fieldOrPropertyInfo._memberInfo;
+        return left is not null && left.Equals(right);
+    }
 
-    public static explicit operator FieldInfo(FieldOrPropertyInfo fieldOrPropertyInfo) =>
-        (FieldInfo)fieldOrPropertyInfo._memberInfo;
+    public static bool operator !=(FieldOrPropertyInfo? left, FieldOrPropertyInfo? right) => !(left == right);
+}
 
-    public static explicit operator PropertyInfo(FieldOrPropertyInfo fieldOrPropertyInfo) =>
-        (PropertyInfo)fieldOrPropertyInfo._memberInfo;
+internal static class FieldOrPropertyInfoExtensions
+{
+    public static FieldOrPropertyInfo? AsFieldOrPropertyInfo(this MemberInfo? memberInfo) =>
+        memberInfo is FieldInfo or PropertyInfo or null ? Unsafe.As<FieldOrPropertyInfo>(memberInfo) : null;
+
+    [return: NotNullIfNotNull(nameof(memberInfo))]
+    public static FieldOrPropertyInfo? ToFieldOrPropertyInfo(this MemberInfo? memberInfo) =>
+        memberInfo is FieldInfo or PropertyInfo or null ? Unsafe.As<FieldOrPropertyInfo>(memberInfo) : throw new InvalidCastException();
+
+    [return: NotNullIfNotNull(nameof(fieldInfo))]
+    public static FieldOrPropertyInfo? AsFieldOrPropertyInfo(this FieldInfo? fieldInfo) =>
+        Unsafe.As<FieldOrPropertyInfo>(fieldInfo);
+
+    [return: NotNullIfNotNull(nameof(propertyInfo))]
+    public static FieldOrPropertyInfo? AsFieldOrPropertyInfo(this PropertyInfo? propertyInfo) =>
+        Unsafe.As<FieldOrPropertyInfo>(propertyInfo);
 }
