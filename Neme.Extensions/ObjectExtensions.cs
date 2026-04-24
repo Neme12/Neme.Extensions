99e6d909b1c415c13e42c0d0beb1d7a14817c9c6
Simona Koníčková <neme112@outlook.com>
2026-04-25 01:48:40 +0200

    Add couple ObjectExtensions

---
+++ Neme.Extensions/ObjectExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Neme.Extensions;
+
+public static class ObjectExtensions
+{
+    extension<T>(T obj)
+        where T : notnull
+    {
+        public string? ToStringInvariant()
+        {
+            return obj is IFormattable formattable
+                ? formattable.ToString(null, CultureInfo.InvariantCulture)
+                : obj.ToString();
+        }
+    }
+
+    extension<T>(T? obj)
+    {
+        public T NotNull()
+        {
+            if (obj is null)
+                throw new UnreachableException("Object must not be null.");
+
+            return obj;
+        }
+    }
+
+    extension<T>(T obj)
+        where T : struct
+    {
+        public T NotDefault()
+        {
+            if (EqualityComparer<T>.Default.Equals(obj, default))
+                throw new UnreachableException("Object must not be the default value.");
+
+            return obj;
+        }
+    }
+}
