51de321b5f6a512db3fc87b1ee9ddd85e6a36504
Simona Koníčková <neme112@outlook.com>
2026-04-25 01:32:42 +0200

    Add UriExtensions

---
+++ Neme.Extensions/UriExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Neme.Extensions;
+
+public static class UriExtensions
+{
+    extension(Uri uri)
+    {
+        public bool IsWebScheme
+        {
+            get
+            {
+                var scheme = uri.Scheme;
+
+                return
+                    scheme.Equals(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase) ||
+                    scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
