9105effa19187f248668973d1cafbbfde4adf68c
Simona Koníčková <neme112@outlook.com>
2026-05-04 01:38:27 +0200

    Add Stackalloc helper

---
+++ Neme.Extensions/Buffers/Stackalloc.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Neme.Extensions.Buffers;
+
+public static class Stackalloc
+{
+    public const int MaxByteLength = 512;
+
+    public static int MaxLength<T>()
+    {
+        var elementSize = Unsafe.SizeOf<T>();
+
+        var maxLength = MaxByteLength / elementSize;
+        if (maxLength < 1)
+            throw new InvalidOperationException($"The maximum byte length is too small to accommodate any elements of type {typeof(T)}.");
+
+        return maxLength;
+    }
+}
