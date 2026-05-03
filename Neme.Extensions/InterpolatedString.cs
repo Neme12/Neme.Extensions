51458e86b925b33ecc8076c695233385d9c0710a
Simona Koníčková <neme112@outlook.com>
2026-05-04 00:15:01 +0200

    Actually, add it now

---
+++ Neme.Extensions/InterpolatedString.cs
@@ -0,0 +1,9 @@
+﻿using Neme.Extensions.CompilerServices;
+
+namespace Neme.Extensions;
+
+public static class InterpolatedString
+{
+    public static string Invariant(ref InvariantInterpolatedStringHandler handler) =>
+        handler.ToStringAndClear();
+}
