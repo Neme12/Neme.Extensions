0b8d2c5469cbec8ed937788c0871e2280f87fe3a
Simona Koníčková <neme112@outlook.com>
2026-05-04 00:17:15 +0200

    Add SemaphoreSlimExtensions.WaitScopeAsync

---
+++ Neme.Extensions/Threading/SemaphoreSlimExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Neme.Extensions.Threading;
+
+public static class SemaphoreSlimExtensions
+{
+    extension(SemaphoreSlim semaphore)
+    {
+        public async Task<Scope> WaitScopeAsync(CancellationToken cancellationToken)
+        {
+            await semaphore.WaitAsync(cancellationToken);
+            return new Scope(semaphore);
+        }
+    }
+
+    public struct Scope : IDisposable
+    {
+        private SemaphoreSlim _semaphore;
+
+        internal Scope(SemaphoreSlim semaphore)
+        {
+            _semaphore = semaphore;
+        }
+
+        public void Dispose()
+        {
+            if (_semaphore is not null)
+            {
+                _semaphore.Release();
+                _semaphore = null!;
+            }
+        }
+    }
+}
