9633102bc702795a55a34b0b110fabe3248f29ca
Simona Koníčková <neme112@outlook.com>
2026-05-04 01:14:26 +0200

    Add HttpResponseExtensions

---
+++ Neme.Extensions.AspNetCore/Http/HttpResponseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Extensions.AspNetCore.Http;
+
+public static class HttpResponseExtensions
+{
+    extension(HttpResponse response)
+    {
+        public async Task<Scope> StartScopeAsync(CancellationToken cancellationToken = default)
+        {
+            await response.StartAsync(cancellationToken);
+            return new Scope(response);
+        }
+    }
+
+    public struct Scope : IAsyncDisposable
+    {
+        private HttpResponse _response;
+
+        internal Scope(HttpResponse response)
+        {
+            _response = response;
+        }
+
+        public readonly HttpResponse Response =>
+            _response;
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_response is not null)
+            {
+                _response = null!;
+                await _response.CompleteAsync();
+            }
+        }
+    }
+}
