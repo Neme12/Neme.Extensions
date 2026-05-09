febf4ab4b43147de37cfc5f0ef0620ab80ee87bd
Simona Koníčková <neme112@outlook.com>
2026-05-09 13:25:07 +0200

    Add Throw helpers from Neme.Utilities

---
+++ Neme.Extensions/Contracts/Throw.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Neme.Utilities.Contracts;
+
+public static class Throw
+{
+    [DoesNotReturn]
+    public static void InvalidOperationException()
+    {
+        throw new InvalidOperationException();
+    }
+
+    [DoesNotReturn]
+    public static void ArgumentException<T>(
+        T argument,
+        string? message = null,
+        [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        throw new ArgumentException(message, paramName: paramName);
+    }
+
+    [DoesNotReturn]
+    public static void ArgumentException<T>(
+        ReadOnlySpan<T> argument,
+        string? message = null,
+        [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        throw new ArgumentException(message, paramName: paramName);
+    }
+
+    [DoesNotReturn]
+    public static void ArgumentOutOfRangeException<T>(
+        T argument,
+        string? message = null,
+        [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        throw new ArgumentOutOfRangeException(paramName, message);
+    }
+}
