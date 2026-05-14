cee3543a9e39f1f64760d574a9a30a0dfa6b33f0
Simona Koníčková <neme112@outlook.com>
2026-05-14 17:06:44 +0200

    Add LazyInitializerExtensions

---
+++ Neme.Extensions/LazyInitializerExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Neme.Extensions.Contracts;
+
+namespace Neme.Extensions;
+
+public static class LazyInitializerExtensions
+{
+    public static T EnsureInitialized<T>(
+        ref Optional<T> target,
+        [NotNullIfNotNull(nameof(syncLock))] ref object? syncLock,
+        Func<T> valueFactory)
+    {
+        Require.ArgumentNotNull(valueFactory);
+
+        if (Volatile.Read(ref Unsafe.AsRef(in Optional.GetHasValueRef(in target))))
+            return target.Value;
+
+        return EnsureInitializedCore(ref target, ref syncLock, valueFactory);
+    }
+
+    private static T EnsureInitializedCore<T>(
+        ref Optional<T> target,
+        [NotNull] ref object? syncLock,
+        Func<T> valueFactory)
+    {
+        Debug.AssertNotNull(valueFactory);
+
+        lock (EnsureLockInitialized(ref syncLock))
+        {
+            if (!Volatile.Read(ref Unsafe.AsRef(in Optional.GetHasValueRef(in target))))
+                target = valueFactory();
+        }
+
+        return target.Value;
+    }
+
+    private static object EnsureLockInitialized([NotNull] ref object? syncLock) =>
+        syncLock ??
+        Interlocked.CompareExchange(ref syncLock, new object(), null) ??
+        syncLock;
+}
