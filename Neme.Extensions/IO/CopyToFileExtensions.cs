3df80ef32cdacf0dc202b8e5d0afb141602ce378
Simona Koníčková <neme112@outlook.com>
2026-05-14 16:39:51 +0200

    Add FileExtensions

---
+++ Neme.Extensions/IO/CopyToFileExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Neme.Extensions.Contracts;
+
+namespace Neme.Extensions.IO;
+
+public static class CopyToFileExtensions
+{
+    public static async Task CopyToFileAsync(this Stream sourceStream, string destFileName, bool overwrite, CancellationToken cancellationToken)
+    {
+        Require.ArgumentNotNull(sourceStream);
+        Require.ArgumentNotNull(destFileName);
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        var destFileStream = FileExtensions.CreateCopyToFileStream(destFileName, overwrite);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        await using (destFileStream.ConfigureAwait(false))
+#else
+        using (destFileStream)
+#endif
+        {
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            await sourceStream.CopyToAsync(destFileStream, cancellationToken).ConfigureAwait(false);
+#else
+            await sourceStream.CopyToAsync(destFileStream, FileExtensions.GetCopyBufferSize(sourceStream), cancellationToken).ConfigureAwait(false);
+#endif
+        }
+    }
+}
---
+++ Neme.Extensions/IO/FileExtensions.cs
@@ -0,0 +1,171 @@
+﻿using Neme.Extensions.Contracts;
+
+namespace Neme.Extensions.IO;
+
+public static class FileExtensions
+{
+    public static FileStream OpenRead(string path, FileOptions options)
+    {
+        Require.ArgumentNotNull(path);
+
+        return CreateFileStream(
+            path,
+            FileMode.Open,
+            FileAccess.Read,
+            FileShare.Read,
+            options);
+    }
+
+    public static FileStream OpenWrite(string path, FileOptions options)
+    {
+        Require.ArgumentNotNull(path);
+
+        return CreateFileStream(
+            path,
+            FileMode.OpenOrCreate,
+            FileAccess.Write,
+            FileShare.None,
+            options);
+    }
+
+    public static FileStream Open(string path, FileMode mode, FileAccess access, FileShare share, FileOptions options)
+    {
+        Require.ArgumentNotNull(path);
+
+        return CreateFileStream(
+            path,
+            mode,
+            access,
+            share,
+            options);
+    }
+
+    public static Task CopyAsync(string sourceFileName, string destFileName, CancellationToken cancellationToken) =>
+        CopyAsync(sourceFileName, destFileName, overwrite: false, cancellationToken);
+
+    public static async Task CopyAsync(string sourceFileName, string destFileName, bool overwrite, CancellationToken cancellationToken)
+    {
+        Require.ArgumentNotNull(sourceFileName);
+        Require.ArgumentNotNull(destFileName);
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        var sourceFileStream = CreateCopyFromFileStream(sourceFileName);
+        var destFileStream = CreateCopyToFileStream(destFileName, overwrite);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        await using (sourceFileStream.ConfigureAwait(false))
+        await using (destFileStream.ConfigureAwait(false))
+#else
+        using (sourceFileStream)
+        using (destFileStream)
+#endif
+        {
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            await sourceFileStream.CopyToAsync(destFileStream, cancellationToken).ConfigureAwait(false);
+#else
+            await sourceFileStream.CopyToAsync(destFileStream, GetCopyBufferSize(sourceFileStream), cancellationToken).ConfigureAwait(false);
+#endif
+        }
+    }
+
+    public static async Task CopyToAsync(string sourceFileName, Stream destStream, CancellationToken cancellationToken)
+    {
+        Require.ArgumentNotNull(sourceFileName);
+        Require.ArgumentNotNull(destStream);
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        var sourceFileStream = CreateCopyFromFileStream(sourceFileName);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        await using (sourceFileStream.ConfigureAwait(false))
+#else
+        using (sourceFileStream)
+#endif
+        {
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            await sourceFileStream.CopyToAsync(destStream, cancellationToken).ConfigureAwait(false);
+#else
+            await sourceFileStream.CopyToAsync(destStream, GetCopyBufferSize(sourceFileStream), cancellationToken).ConfigureAwait(false);
+#endif
+        }
+    }
+
+    private static FileStream CreateCopyFromFileStream(string path)
+    {
+        Assert.NotNull(path);
+
+        return CreateFileStream(
+            path,
+            FileMode.Open,
+            FileAccess.Read,
+            FileShare.Read,
+            FileOptions.SequentialScan | FileOptions.Asynchronous);
+    }
+
+    internal static FileStream CreateCopyToFileStream(string path, bool overwrite)
+    {
+        Assert.NotNull(path);
+
+        return CreateFileStream(
+            path,
+            overwrite ? FileMode.Create : FileMode.CreateNew,
+            FileAccess.Write,
+            FileShare.None,
+            FileOptions.SequentialScan | FileOptions.Asynchronous);
+    }
+
+    private static FileStream CreateFileStream(
+        string path,
+        FileMode mode,
+        FileAccess access,
+        FileShare share,
+        FileOptions options)
+    {
+        Assert.NotNull(path);
+
+#if NET6_0_OR_GREATER
+        return new(path, new FileStreamOptions { Mode = mode, Access = access, Share = share, Options = options });
+#else
+        const int DefaultBufferSize = 4096;
+        return new (path, mode, access, share, DefaultBufferSize, options);
+#endif
+    }
+
+#if !(NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
+    internal static int GetCopyBufferSize(Stream stream)
+    {
+        Assert.NotNull(stream);
+
+        const int DefaultCopyBufferSize = 81920;
+
+        int bufferSize = DefaultCopyBufferSize;
+
+        if (stream.CanSeek)
+        {
+            long length = stream.Length;
+            long position = stream.Position;
+            if (length <= position) // Handles negative overflows
+            {
+                // There are no bytes left in the stream to copy.
+                // However, because CopyTo{Async} is virtual, we need to
+                // ensure that any override is still invoked to provide its
+                // own validation, so we use the smallest legal buffer size here.
+                bufferSize = 1;
+            }
+            else
+            {
+                long remaining = length - position;
+                if (remaining > 0)
+                {
+                    // In the case of a positive overflow, stick to the default size
+                    bufferSize = (int)Math.Min(bufferSize, remaining);
+                }
+            }
+        }
+
+        return bufferSize;
+    }
+#endif
+}
