358df06b6ca7230ed154cc9e3ebadba2a2cc2b3a
Simona Koníčková <neme112@outlook.com>
2026-05-15 11:00:31 +0200

    Add ExceptionExtensions

---
+++ Neme.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Neme.Extensions;
+
+internal static class ExceptionExtensions
+{
+    extension(Exception)
+    {
+        public static T TryCatch<T, TException>(Func<T> action, Func<TException, T> handler)
+            where TException : Exception
+        {
+            try
+            {
+                return action();
+            }
+            catch (TException ex)
+            {
+                return handler(ex);
+            }
+        }
+    }
+}
