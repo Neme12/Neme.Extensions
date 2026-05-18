67eed1d6206c716171e6ea7889c01601b7ae8e55
Simona Koníčková <neme112@outlook.com>
2026-05-18 17:13:27 +0200

    Add PropertyInfo.GetValue extensions

---
+++ Neme.Extensions/Reflection/PropertyInfoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Neme.Extensions.Reflection;
+
+public static class PropertyInfoExtensions
+{
+    extension(PropertyInfo property)
+    {
+        public object? GetValue<TValue>(object? obj) =>
+            (TValue)property.GetValue(obj)!;
+
+        public object? GetValue<TValue>(object? obj, object?[]? index) =>
+            (TValue)property.GetValue(obj, index)!;
+
+        public object? GetValue<TValue>(object? obj, BindingFlags invokeAttr, Binder? binder, object?[]? index, CultureInfo? culture) =>
+            (TValue)property.GetValue(obj, invokeAttr, binder, index, culture)!;
+    }
+}
