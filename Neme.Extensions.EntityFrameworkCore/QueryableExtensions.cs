b6023ab9cbf78537c0785e2f184d0178d75a7191
Simona Koníčková <neme112@outlook.com>
2026-05-20 11:26:24 +0200

    Add ToImmutableArrayAsync

---
+++ Neme.Extensions.EntityFrameworkCore/QueryableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Neme.Extensions.Linq;
+
+namespace Neme.Extensions.EntityFrameworkCore;
+
+public static class QueryableExtensions
+{
+    extension<TSource>(IQueryable<TSource> source)
+    {
+        public async Task<ImmutableArray<TSource>> ToImmutableArrayAsync(
+            CancellationToken cancellationToken = default)
+        {
+            var builder = source.TryGetNonEnumeratedCount2(out var count)
+                ? ImmutableArray.CreateBuilder<TSource>(count)
+                : ImmutableArray.CreateBuilder<TSource>();
+
+            await foreach (var item in source.AsAsyncEnumerable().WithCancellation(cancellationToken).ConfigureAwait(false))
+                builder.Add(item);
+
+            return builder.DrainToImmutable();
+        }
+    }
+}
