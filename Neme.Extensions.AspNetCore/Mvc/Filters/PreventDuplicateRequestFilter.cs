b6c46c8a52fc8dfe1af8173ff65f9ac6659add90
Simona Koníčková <neme112@outlook.com>
2026-05-20 13:51:00 +0200

    Separate PreventDuplicateRequestFilter

---
+++ Neme.Extensions.AspNetCore/Mvc/Filters/PreventDuplicateRequestFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Neme.Extensions.AspNetCore.Mvc.Filters;
+
+public sealed class PreventDuplicateRequestFilter : IAsyncActionFilter
+{
+    internal static async Task BeforeActionExecutionAsync(ActionExecutingContext context)
+    {
+        var httpContext = context.HttpContext;
+        var cancellationToken = httpContext.RequestAborted;
+
+        if (!httpContext.Request.Form.ContainsKey("__RequestVerificationToken"))
+            return;
+
+        var session = httpContext.Session;
+
+        await session.LoadAsync(cancellationToken);
+
+        var currentToken = httpContext.Request.Form["__RequestVerificationToken"].ToString();
+        var lastToken = session.GetString("__LastProcessedToken");
+
+        if (lastToken == currentToken)
+        {
+            context.ModelState.AddModelError(string.Empty, Strings.PreventDuplicateRequestAttribute_RequestSentRepeatedly);
+            return;
+        }
+
+        session.SetString("__LastProcessedToken", currentToken);
+
+        await session.CommitAsync(cancellationToken);
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        await BeforeActionExecutionAsync(context);
+        await next();
+    }
+}
