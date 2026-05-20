61e04ad8ddbee0dd9f7c360efc9109d734372ebd
Simona Koníčková <neme112@outlook.com>
2026-05-20 13:03:43 +0200

    Add strongly typed AggregateLogger

---
+++ Neme.Extensions.MicrosoftExtensions/Logging/AggregateLogger`1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Neme.Extensions;
+
+namespace Neme.Extensions.MicrosoftExtensions.Logging;
+
+public sealed class AggregateLogger<T> : ILogger<T>
+{
+    private readonly ImmutableArray<ILogger<T>> _loggers;
+
+    public AggregateLogger(ImmutableArray<ILogger<T>> loggers)
+    {
+        _loggers = loggers;
+    }
+
+    public AggregateLogger(IEnumerable<ILogger<T>> loggers)
+    {
+        _loggers = loggers.ToImmutableArray();
+    }
+
+    public AggregateLogger(params ReadOnlySpan<ILogger<T>> loggers)
+    {
+        _loggers = loggers.ToImmutableArray();
+    }
+
+#if NET8_0_OR_GREATER
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+#else
+    public IDisposable BeginScope<TState>(TState state)
+#endif
+    {
+        var scopes = ImmutableArray.CreateBuilder<IDisposable?>(_loggers.Length);
+
+        foreach (var logger in _loggers)
+            scopes.Add(logger.BeginScope(state));
+
+        return new AggregateScope(scopes.MoveToImmutable());
+
+    }
+    public bool IsEnabled(LogLevel logLevel) =>
+        _loggers.Any(logger => logger.IsEnabled(logLevel));
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        foreach (var logger in _loggers)
+            logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+}
