49dfbfd01170c640fc0cdfdc282650f0563a4a17
Simona Koníčková <neme112@outlook.com>
2026-05-22 20:22:58 +0200

    Try removing ModuleInitializer for netfx

--- Neme.Extensions.Tests/TestCultureInitializer.cs
+++ Neme.Extensions.Tests/TestCultureInitializer.cs
@@ -1,14 +1,22 @@
 using System.Globalization;
+#if NET5_0_OR_GREATER
 using System.Runtime.CompilerServices;
+#endif
 
 namespace Neme.Extensions.Tests;
 
 internal static class TestCultureInitializer
 {
+#if NET5_0_OR_GREATER
 #pragma warning disable CA2255
     [ModuleInitializer]
 #pragma warning restore CA2255
     internal static void Initialize()
+#else
+    // For .NET Framework, this needs to be called manually or via IClassFixture/ICollectionFixture
+    // Since we can't use ModuleInitializer on .NET Framework 4.8
+    internal static void Initialize()
+#endif
     {
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
         CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
