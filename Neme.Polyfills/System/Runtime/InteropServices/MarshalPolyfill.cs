29b1caf9ec3fbaa165295c40e584eecdbb6674b5
Simona Koníčková <neme112@outlook.com>
2026-05-23 16:30:25 +0200

    Add polyfill for Marshal.GetLastPInvokeError

---
+++ Neme.Polyfills/System/Runtime/InteropServices/MarshalPolyfill.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Neme.Polyfills.System.Runtime.InteropServices;
+
+#if !NET6_0_OR_GREATER
+
+public static class MarshalPolyfill
+{
+    extension(Marshal)
+    {
+        public static int GetLastPInvokeError() =>
+            Marshal.GetLastWin32Error();
+    }
+}
+
+#endif
