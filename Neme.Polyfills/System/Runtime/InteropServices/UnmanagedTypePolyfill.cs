c9a5c2dcfb84518b80ea1c504cc7dd82ad103c3f
Simona Koníčková <neme112@outlook.com>
2026-05-27 15:38:38 +0200

    Fix polyfill conditional compilation

--- Neme.Polyfills/System/Runtime/InteropServices/UnmanagedTypePolyfill.cs
+++ Neme.Polyfills/System/Runtime/InteropServices/UnmanagedTypePolyfill.cs
@@ -1,13 +1,13 @@
 ﻿namespace System.Runtime.InteropServices;
 
-#if !(NET47_OR_GREATER || NETCOREAPP)
 public static class UnmanagedTypePolyfill
 {
     public const UnmanagedType LPUTF8Str = (UnmanagedType)48;
 
+#if !(NET47_OR_GREATER || NETCOREAPP3_0_OR_GREATER)
     extension(UnmanagedType)
     {
         public static UnmanagedType LPUTF8Str => (UnmanagedType)48;
     }
-}
 #endif
+}
