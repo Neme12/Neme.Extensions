6f478f71019d2e9f938df66d143ff59bdd16d901
Simona Koníčková <neme112@outlook.com>
2026-05-27 16:03:05 +0200

    Rename OwnershipTransferIf to OwnershipTransferWhen

--- Neme.Extensions/Ownership/OwnershipTransferWhenAttribute.cs
+++ Neme.Extensions/Ownership/OwnershipTransferWhenAttribute.cs
@@ -1,13 +1,13 @@
 ﻿namespace Neme.Extensions.Ownership;
 
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false, Inherited = true)]
-public sealed class OwnershipTransferIfAttribute : Attribute
+public sealed class OwnershipTransferWhenAttribute : Attribute
 {
     /// <param name="parameterName">
     /// Name of the parameter that if <c>false</c>, the ownership transfer does not occur.
     /// This is useful for <c>ownsValue</c> parameters.
     /// </param>
-    public OwnershipTransferIfAttribute(string parameterName)
+    public OwnershipTransferWhenAttribute(string parameterName)
     {
         ParameterName = parameterName;
     }
