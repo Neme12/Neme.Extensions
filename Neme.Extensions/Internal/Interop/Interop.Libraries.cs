0cce38a0ba72455f21cd33f2a5ce6b58d49d04e0
Simona Koníčková <neme112@outlook.com>
2026-05-30 12:58:28 +0200

    Foo3

--- Neme.Extensions/Internal/Interop/Interop.Libraries.cs
+++ Neme.Extensions/Internal/Interop/Interop.Libraries.cs
@@ -5,6 +5,6 @@ internal static partial class Interop
     internal static class Libraries
     {
         internal const string libc = "libc";
-        internal const string neme_macos_shim = "libneme_macos_shim.dylib";
+        internal const string neme_macos_shim = "neme_macos_shim";
     }
 }
