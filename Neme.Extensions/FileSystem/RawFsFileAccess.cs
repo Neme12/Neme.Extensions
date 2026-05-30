31a8b9072d0325e386e916d4f77cd7f084dfbf81
Simona Koníčková <neme112@outlook.com>
2026-05-30 10:36:04 +0200

    Add comment

--- Neme.Extensions/FileSystem/RawFsFileAccess.cs
+++ Neme.Extensions/FileSystem/RawFsFileAccess.cs
@@ -1,5 +1,11 @@
 ﻿namespace Neme.Extensions.FileSystem;
 
+/// <summary>
+/// Raw counterpart to <see cref="FsFileAccess"/> without any combined flags.
+/// The main enum has combined flags, and is therefore unusable for bitwise operations.
+/// This enum is intended to be used for flag checks instead of the main enum, and
+/// is not intended to be used by users of the library.
+/// </summary>
 [Flags]
 internal enum RawFsFileAccess
 {
