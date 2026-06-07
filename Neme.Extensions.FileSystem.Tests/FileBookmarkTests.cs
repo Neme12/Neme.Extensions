ab8c4137ebd26d0220fe25ba342320a7d05ded9c
Simona Koníčková <neme112@outlook.com>
2026-06-07 19:49:22 +0200

    Add FileBookmark struct

---
+++ Neme.Extensions.FileSystem.Tests/FileBookmarkTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Neme.Extensions.FileSystem.Tests;
+
+public sealed class FileBookmarkTests
+{
+    [Fact]
+    public void Constructor_SetsPathAndFileId()
+    {
+        var fileId = FileId.FromUnixId(new FileId.UnixId(123UL, 456UL));
+
+        var sut = new FileBookmark("/tmp/test.txt", fileId);
+
+        Assert.Equal("/tmp/test.txt", sut.Path);
+        Assert.Equal(fileId, sut.FileId);
+    }
+
+    [Fact]
+    public void EqualInstances_AreEqual()
+    {
+        var fileId = FileId.FromWindowsId(new FileId.WindowsId(1UL, 2UL, 3UL));
+        var left = new FileBookmark("C:/temp/test.txt", fileId);
+        var right = new FileBookmark("C:/temp/test.txt", fileId);
+
+        Assert.Equal(left, right);
+    }
+}
---
+++ Neme.Extensions.FileSystem/FileBookmark.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Neme.Extensions.FileSystem;
+
+[StructLayout(LayoutKind.Auto)]
+public readonly record struct FileBookmark(string Path, FileId FileId);
