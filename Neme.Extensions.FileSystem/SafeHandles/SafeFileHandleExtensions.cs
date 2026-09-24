90c66fdeb0d9138101e5d3d2a66352ad586a6efb
Simona Koníčková <neme112@outlook.com>
2026-09-24 23:20:19 +0200

    Add SafeFileHandleExtensions

---
+++ Neme.Extensions.FileSystem/SafeHandles/SafeFileHandleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace Neme.Extensions.FileSystem.SafeHandles;
+
+public static class SafeFileHandleExtensions
+{
+    extension(SafeFileHandle file)
+    {
+        public string Path =>
+            FileIO.GetPath(file);
+
+        public FileAccess Access =>
+            FileIO.GetAccess(file);
+
+        public bool CanRead =>
+            FileIO.GetAccess(file).HasFlag(FileAccess.Read);
+
+        public bool CanWrite =>
+            FileIO.GetAccess(file).HasFlag(FileAccess.Write);
+    }
+}
