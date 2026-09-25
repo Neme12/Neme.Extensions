792c63fdf42962ee31fc8536084730c7279cec3a
Simona Koníčková <neme112@outlook.com>
2026-09-25 22:51:41 +0200

    Try using new DirectoryInfo().FullName

--- Neme.Extensions.FileSystem.Tests/SafeHandles/SafeFileHandleExtensionsTests.cs
+++ Neme.Extensions.FileSystem.Tests/SafeHandles/SafeFileHandleExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using Neme.Extensions.FileSystem.SafeHandles;
+using Neme.Extensions.FileSystem.Tests.TestUtilities;
 using Neme.Extensions.IO;
 using IOPath = System.IO.Path;
 
@@ -14,7 +15,7 @@ public sealed class SafeFileHandleExtensionsTests
         public void ValidFileHandle_ReturnsFullPath()
         {
             // Arrange
-            var expected =  IOPath.GetFullPath($"{System.IO.Path.GetTempPath()}{nameof(SafeFileHandleExtensionsTests)}_{Guid.NewGuid():N}.tmp");
+            var expected = $"{PathUtilities.Normalize(IOPath.GetTempPath())}{nameof(SafeFileHandleExtensionsTests)}_{Guid.NewGuid():N}.tmp";
             string result;
             using (var tempFile = FileIO.OpenHandle(expected, FileOpenRequest.CreateNew(FileSystemAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete, FileOptions.DeleteOnClose, FileAttributes.Temporary)))
             {
---
+++ Neme.Extensions.FileSystem.Tests/TestUtilities/PathUtilities.cs
@@ -0,0 +1,7 @@
+﻿namespace Neme.Extensions.FileSystem.Tests.TestUtilities;
+
+internal static class PathUtilities
+{
+    public static string Normalize(string path) =>
+        new DirectoryInfo(path).FullName;
+}
