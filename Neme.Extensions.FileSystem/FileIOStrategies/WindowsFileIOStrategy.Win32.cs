1d2ee5b9970bb0f26fb2d773a6a422e3e28964a5
Simona Koníčková <neme112@outlook.com>
2026-09-25 17:57:56 +0200

    Use GETFINALPATHNAMEBYHANDLE_FLAGS.FILE_NAME_OPENED

--- Neme.Extensions.FileSystem/FileIOStrategies/WindowsFileIOStrategy.Win32.cs
+++ Neme.Extensions.FileSystem/FileIOStrategies/WindowsFileIOStrategy.Win32.cs
@@ -80,8 +80,11 @@ internal sealed partial class WindowsFileIOStrategy : FileIOStrategy
 
         while (true)
         {
-            charsWritten = PInvoke.GetFinalPathNameByHandle(file, bufferLease.Buffer,
-                GETFINALPATHNAMEBYHANDLE_FLAGS.VOLUME_NAME_DOS);
+            charsWritten = PInvoke.GetFinalPathNameByHandle(
+                file,
+                bufferLease.Buffer,
+                GETFINALPATHNAMEBYHANDLE_FLAGS.VOLUME_NAME_DOS |
+                GETFINALPATHNAMEBYHANDLE_FLAGS.FILE_NAME_OPENED);
             if (charsWritten != bufferLease.Length)
                 break;
 
